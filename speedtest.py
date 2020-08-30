6c262d6803669e2b527b6dcc805b746da7595c77
armanrahaman <34883730+armanrahaman@users.noreply.github.com>
2020-08-30 09:47:15 +0000

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -2,9 +2,9 @@ from speedtest import Speedtest
 
 st = Speedtest()
 
-print("your download speed is: ", st.download())
+print("your download speed : ", st.download())
 
-print("your upload speed is: ", st.upload())
+print("your upload speed : ", st.upload())
 
 st.get_servers([])
 print('ping:', st.results.ping)
