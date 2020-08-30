269d42ce78199b176d5fc3cddf74106329e639c4
H.Arman <64675035+Arman170616@users.noreply.github.com>
2020-08-30 13:40:24 +0600

    Create speedtest.py
    Test your internet speed using python.
    
    install package => pip install speedtest-cli
    
    then run this code and show your internet download, upload and ping speed..

---
+++ speedtest.py
@@ -0,0 +1,10 @@
+from speedtest import Speedtest
+
+st = Speedtest()
+
+print("your download speed is: ", st.download())
+
+print("your upload speed is: ", st.upload())
+
+st.get_servers([])
+print('ping:', st.results.ping)
